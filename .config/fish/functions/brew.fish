981aa1fe811e67303d199cec142f7fc033350364
Joseph Earl <joe@josephearl.co.uk>
2017-02-24 21:28:28 +0000

    Improve brew function

--- .config/fish/functions/brew.fish
+++ .config/fish/functions/brew.fish
@@ -1,7 +1,17 @@
 function brew
   set brew_path $PATH
-  set -e brew_path[1]
-  set -e brew_path[1]
+  set i 1
+  for p in $brew_path
+    if [ "$p" = "$PYENV_DIR/shims" ]
+      set -e brew_path[$i]  
+      set i (math "$i-1")
+    end
+    if [ "$p" = "$RBENV_DIR/shims" ]
+      set -e brew_path[$i]
+      set i (math "$i-1")
+    end
+    set i (math "$i+1")
+  end
   begin
     set -lx PATH $brew_path
     command brew $argv
