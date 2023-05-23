e54565238e9f8a1e8413d07cebfe46ca2e818226
Joseph Earl <hello@josephearl.co.uk>
2023-05-23 09:58:24 +0100

    Update asdf and use global .tool-versions file to manage default versions

--- .install/asdf/install.bash
+++ .install/asdf/install.bash
@@ -36,21 +36,4 @@ test -d "$ASDF_DATA_DIR/plugins/ruby" || {
   asdf plugin add ruby
 }
 
-asdf install dotnet-core 3.1.425
-asdf global dotnet-core 3.1.425
-
-asdf install golang 1.19.3
-asdf global golang 1.19.3
-
-asdf install java temurin-17.0.5+8
-asdf global java temurin-17.0.5+8
-
-asdf install nodejs 18.12.1
-asdf global nodejs 18.12.1
-
-asdf install python 3.11.0
-asdf install python 2.7.18
-asdf global python 3.11.0 2.7.18
-
-asdf install ruby 3.1.2
-asdf global ruby 3.1.2
+(cd "$HOME"; asdf install)
